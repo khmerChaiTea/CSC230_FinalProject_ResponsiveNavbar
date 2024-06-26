92bed2cb2034713c59b8c42fd2f79da717972678
pchai <prempreychhay@gmail.com>
2024-06-26 12:46:14 -0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
 });
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    setTimeout(() => {
+      const responseMessage = document.getElementById("responseMessage");
+      responseMessage.style.display = "block";
+      responseMessage.innerHTML = `<p>Thank you, ${name}! Your message has been sent successfully.</p>`;
+
+      document.getElementById("contactForm").reset();
+    }, 500);
+  });
